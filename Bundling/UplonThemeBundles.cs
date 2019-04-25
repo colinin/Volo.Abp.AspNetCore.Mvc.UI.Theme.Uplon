33536b2cf1df3ffb14206eb66626c49a8fdd8f3e
colinin <colin.in@foxmail.com>
2019-04-25 09:19:28 +0800

    The uplon theme style of the abp framework

---
+++ Bundling/UplonThemeBundles.cs
@@ -0,0 +1,45 @@
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Uplon.Bundling
+{
+    public static class UplonThemeBundles
+    {
+        public static class Styles
+        {
+            public const string Global = "Uplon.Global";
+
+            /// <summary>
+            /// C3图表控件
+            /// </summary>
+            public const string C3Chart = "Uplon.C3Chart";
+            /// <summary>
+            /// C3图表控件
+            /// </summary>
+            public const string MinC3Chart = "Uplon.C3Chart.Min";
+            /// <summary>
+            /// JsTree
+            /// </summary>
+            public const string JsTree = "Uplon.JsTree";
+        }
+
+        public static class Scripts
+        {
+            public const string Global = "Uplon.Global";
+
+            /// <summary>
+            /// C3图表控件
+            /// </summary>
+            public const string C3Chart = "Uplon.C3Chart";
+            /// <summary>
+            /// C3图表控件
+            /// </summary>
+            public const string MinC3Chart = "Uplon.C3Chart.Min";
+            /// <summary>
+            /// JsTree
+            /// </summary>
+            public const string JsTree = "Uplon.JsTree";
+            /// <summary>
+            /// JsTree
+            /// </summary>
+            public const string MinJsTree = "Uplon.JsTree.Min";
+        }
+    }
+}
